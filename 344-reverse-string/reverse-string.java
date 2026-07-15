eae74e72f37e98bca7198bf73051961a99100193
Ayush Nath Goswami <cagoswami35@gmail.com>
2026-07-15 22:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (37.79%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,16 +1,26 @@
 class Solution {
     public void reverseString(char[] s) {
-        helper(s,0,s.length-1);
+        //helper(s,0,s.length-1);
+        int high=s.length-1;
+        int low=0;
+        while(low<=high){
+            char temp=s[low];
+            s[low]=s[high];
+            s[high]=temp;
+
+            low++;
+            high--;
+        }
         
     }
-    private void helper(char[] s,int low,int high){
-        if(low>=high){
-            return; 
-        }
-        char temp=s[low];
-        s[low]=s[high];
-        s[high]=temp;
+    // private void helper(char[] s,int low,int high){
+    //     if(low>=high){
+    //         return; 
+    //     }
+    //     char temp=s[low];
+    //     s[low]=s[high];
+    //     s[high]=temp;
 
-        helper(s,low+1,high-1);
-    }
+    //     helper(s,low+1,high-1);
+    // }
 }
