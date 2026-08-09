52e444618cff41d3cfd140686785381da18d9245
Ayush Nath Goswami <cagoswami35@gmail.com>
2026-08-09 15:19:33 +0530

    Time: 1 ms (61.30%) | Memory: 42.4 MB (52.82%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -2,9 +2,10 @@ class Solution {
     public int reverseBits(int n) {
         int result=0;
         for(int i=0;i<32;i++){
-            result<<=1;
+            result<<=1;//creating space for the incoming bits
 
-            result|=(n&1);
+            result|=(n&1);// AND is used to fetch the LSB from the number
+                         // OR is used to append the newly fetched lsb to the result 
             n>>=1;
 
 
